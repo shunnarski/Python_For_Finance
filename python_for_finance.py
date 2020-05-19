717db02ae18f8987650986bce3f738ea97139c85
shunnarski <ashunnarah9597@gmail.com>
2020-05-18 22:15:42 -0500

    pff part 4

--- python_for_finance.py
+++ python_for_finance.py
@@ -1,6 +1,9 @@
 import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
+from mplfinance.original_flavor import candlestick_ohlc
+# import mplfinance as mpf
+import matplotlib.dates as mdates
 import pandas as pd
 import pandas_datareader.data as web
 
@@ -15,13 +18,25 @@ style.use('ggplot')
 
 df = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
 
-df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+# df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+
+# get every 10 days
+df_ohlc = df['Adj Close'].resample('10D').ohlc()
+df_volume = df['Volume'].resample('10D').sum()
+
+df_ohlc.reset_index(inplace=True)
+df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
+
 
 ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
 ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
+ax1.xaxis_date()
 
-ax1.plot(df.index, df['Adj Close'])
-ax1.plot(df.index, df['100ma'])
-ax2.bar(df.index, df['Volume'])
+candlestick_ohlc(ax1, df_ohlc.values, width=2, colorup='g', colordown='r')
+ax2.fill_between(df_volume.index.map(mdates.date2num), df_volume.values, 0)
+
+# ax1.plot(df.index, df['Adj Close'])
+# ax1.plot(df.index, df['100ma'])
+# ax2.bar(df.index, df['Volume'])
 
 plt.show()
