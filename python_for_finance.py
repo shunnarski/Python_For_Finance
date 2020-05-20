411872e71d04d3eebd9b0513bfc9951bfbd251a9
shunnarski <ashunnarah9597@gmail.com>
2020-05-20 08:10:29 -0500

    pff part 6

--- python_for_finance.py
+++ python_for_finance.py
@@ -1,42 +1,56 @@
+import bs4 as bs
+import pickle
+import requests
 import datetime as dt
+import os
+import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import style
-from mplfinance.original_flavor import candlestick_ohlc
-# import mplfinance as mpf
-import matplotlib.dates as mdates
-import pandas as pd
 import pandas_datareader.data as web
 
-style.use('ggplot')
+def save_sp500_tickers():
+    resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup = bs.BeautifulSoup(resp.text, 'lxml')
+    table = soup.find('table', {'class': 'wikitable sortable'})
 
-# start = dt.datetime(2000, 1, 1)
-# end = dt.datetime(2016, 12, 31)
+    tickers = []
 
-# # include data reader for a stock ticker => TSLA: Tesla
-# df = web.DataReader('TSLA', 'yahoo', start, end)
-# df.to_csv('tsla.csv')
+    for row in table.findAll('tr')[1:]:
+        ticker = row.findAll('td')[0].text
+        tickers.append(ticker.replace('\n', ''))
 
-df = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
+    with open("sp500tickers.pickle", "wb") as f:
+        pickle.dump(tickers, f)
+    print(tickers)
+    return tickers
 
-# df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
-
-# get every 10 days
-df_ohlc = df['Adj Close'].resample('10D').ohlc()
-df_volume = df['Volume'].resample('10D').sum()
-
-df_ohlc.reset_index(inplace=True)
-df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
+save_sp500_tickers()
 
 
-ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
-ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
-ax1.xaxis_date()
+def get_data_from_yahoo(reload_sp500=False):
+    if reload_sp500:
+        tickers = save_sp500_tickers()
+    else:
+        with open("sp500tickers.pickle", "rb") as f:
+            tickers = pickle.load(f)
+    
+    # create directory in os for storing the stock data locally
+    if not os.path.exists('stock_dfs'):
+        os.makedirs('stock_dfs')
 
-candlestick_ohlc(ax1, df_ohlc.values, width=2, colorup='g', colordown='r')
-ax2.fill_between(df_volume.index.map(mdates.date2num), df_volume.values, 0)
+    start = dt.datetime(2000,1,1)
+    end = dt.datetime(2016, 12, 31)
 
-# ax1.plot(df.index, df['Adj Close'])
-# ax1.plot(df.index, df['100ma'])
-# ax2.bar(df.index, df['Volume'])
+    for ticker in tickers:
+        try:
+            print(ticker)
+            if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+                df = web.DataReader(ticker, 'yahoo', start, end)
+                df.to_csv('stock_dfs/{}.csv'.format(ticker))
+            else:
+                print('Already have {}'.format(ticker))
+        except:
+            pass
+    
 
-plt.show()
+get_data_from_yahoo()
