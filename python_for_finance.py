e0731211fba688ba7bc4fdf35ae964c3a27ed793
shunnarski <ashunnarah9597@gmail.com>
2020-05-20 08:46:14 -0500

    pff part 8

--- python_for_finance.py
+++ python_for_finance.py
@@ -5,8 +5,11 @@ import datetime as dt
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib import style
 import pandas_datareader.data as web
+
+style.use('ggplot')
 
 def save_sp500_tickers():
     resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
@@ -21,11 +24,8 @@ def save_sp500_tickers():
 
     with open("sp500tickers.pickle", "wb") as f:
         pickle.dump(tickers, f)
-    print(tickers)
+
     return tickers
-
-save_sp500_tickers()
-
 
 def get_data_from_yahoo(reload_sp500=False):
     if reload_sp500:
@@ -52,5 +52,55 @@ def get_data_from_yahoo(reload_sp500=False):
         except:
             pass
     
+def compile_data():
+    with open('sp500tickers.pickle', 'rb') as f:
+        tickers = pickle.load(f)
+    
+    main_df = pd.DataFrame()
 
-get_data_from_yahoo()
+    for count, ticker in enumerate(tickers):
+        if os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+            df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+            df.set_index('Date', inplace=True)
+            df.rename(columns={'Adj Close': ticker}, inplace=True)
+            df.drop(['Open', 'High', 'Low', 'Close', 'Volume'], 1, inplace=True)
+
+            if main_df.empty:
+                main_df = df
+            else:
+                main_df = main_df.join(df, how='outer')
+
+            if count % 10 == 0:
+                print(count)
+
+    main_df.to_csv('sp500_joined_closes.csv')
+
+def visualize_data():
+    df = pd.read_csv('sp500_joined_closes.csv')
+    df_corr = df.corr()
+
+    data = df_corr.values
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+
+    heatmap = ax.pcolor(data, cmap=plt.cm.RdYlGn)
+    fig.colorbar(heatmap)
+    ax.set_xticks(np.arange(data.shape[0]) + 0.5, minor=False)
+    ax.set_yticks(np.arange(data.shape[1]) + 0.5, minor=False)
+
+    ax.invert_yaxis()
+    ax.xaxis.tick_top()
+
+    column_labels = df_corr.columns
+    row_labels = df_corr.index
+    
+    ax.set_xticklabels(column_labels)
+    ax.set_yticklabels(row_labels)
+    plt.xticks(rotation=90)
+    heatmap.set_clim(-1,1)
+    plt.tight_layout()
+
+    plt.show()
+
+
+visualize_data()
